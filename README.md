5599489231bdba4c3ca7ba6c6f74bb63735fe6cf
Michal <michalldbs@seznam.cz>
2023-04-13 21:42:21 +0200

    Změna github url

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vygooglim.eu
+
+Český překlad z repozitáře https://github.com/NatoBoram/lmgtfy
--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,7 @@
 <footer
 	class="flex flex-row items-center gap-4 bg-zinc-200 p-4 text-zinc-500 dark:bg-zinc-900 dark:text-zinc-400"
 >
-	<a class="flex flex-row items-center gap-2" href="https://github.com/NatoBoram/lmgtfy">
+	<a class="flex flex-row items-center gap-2" href="https://github.com/mdobes/vygooglim/">
 		<CodeBracket />
 		GitHub
 	</a>
